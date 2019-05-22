b1623b1bd7b8c63e9c174551c1b6888769ff0c16
Miguel Arinto <a37552@alunos.ipb.pt>
2019-05-22 18:48:37 +0100

    Debug

--- _PL/Admin/adminclientes.php
+++ _PL/Admin/adminclientes.php
@@ -2,7 +2,7 @@
 <html lang="pt-pt">
 <head>
     <?php
-    include '../_includes/Head.php';
+    include_once 'C:/xampp/htdocs/DWphp/_PL/_includes/Head.php';
     ?>
     <link href="../../_css/admingames.css" type="text/css" rel="stylesheet"/>
 
@@ -29,7 +29,7 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title">Modal title</h5>
+                                    <h5>Add Client</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -39,22 +39,17 @@
                                         <div class="form-group">
                                             <label for="exampleInputEmail1">Email address</label>
                                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleInputPassword1">Password</label>
                                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                                         </div>
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Submit</button>
+
                                     </form>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save</button>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
                             </div>
                         </div>
