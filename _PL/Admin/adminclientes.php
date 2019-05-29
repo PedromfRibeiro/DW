68dbd8c42c764ff94b7e705658ff3ec2379a2bed
Miguel Arinto <a37552@alunos.ipb.pt>
2019-05-29 16:46:39 +0100

    Update project+Usercontroll

--- _PL/Admin/adminclientes.php
+++ _PL/Admin/adminclientes.php
@@ -21,7 +21,43 @@
             </tr>
             </thead>
             <?php
+
             require_once 'C:/xampp/htdocs/DWphp/_BL/Utilizador.php';
+            if(isset($_GET['pagina']) && !empty($_GET['pagina'])){
+                $currentPage = $_GET['pagina'];
+            }else{
+                $currentPage = $_POST["pagina"];
+            }
+            $amountofrecords=4;
+            $initialrecord=$currentPage*$amountofrecords;
+            $lastrecord=($initialrecord+$amountofrecords);
+            $POD = new Utilizador('', '', '', '', '', ' ', ' ', ' ', ' ');
+            $BFetch = ($POD->ReadALL());
+            for ($recordcounter = 0; $recordcounter<=$lastrecord && $recordcounter>=$initialrecord && $row = $BFetch->fetch() ; $recordcounter++) {
+                    echo '<tr>';
+
+                    echo '<td>' . $row['Nome'] . '</td>';
+                    echo '<td>' . $row['Data_Registo'] . '</td>';
+                    echo '<td>' . $row['Autorizacao'] . '</td>';
+                    echo '<td>' . $row['Data_Nascimento'] . '</td>';
+                    echo '<td>' . $row['email'] . '</td>';
+                    echo '<td>' . $row['code_hash'] . '</td>';
+                    echo '<td>' . $row['Verify'] . '</td>';
+
+                    echo '<td>';
+                    echo '<a href="#editUtilizador" name="editUtilizador" class="edit"  data-toggle="modal" data-target="#myModal"><i class="material-icons" title="Edit" >&#xE254;</i></a>';
+                    echo '<a href="#deleteUtiizador" name="deleteUtilizador" class="delete"><i class="material-icons" title="Delete">&#xE872;</i></a>';
+                    echo '</td>';
+                    echo '</tr>';
+
+
+                }
+                $BFetch->closeCursor();
+                $POD = null;
+
+
+
+            /*require_once 'C:/xampp/htdocs/DWphp/_BL/Utilizador.php';
             $POD = new Utilizador('', '', '', '', '', ' ', ' ', ' ', ' ');
             $BFetch = ($POD->ReadALL());
             while ($row = $BFetch->fetch()) {
@@ -37,14 +73,14 @@
 
                 echo '<td>';
                 echo '<a  href="#editUtilizador" name="editUtilizador" class="edit"  data-toggle="modal" data-target="#myModal"><i class="material-icons" title="Edit" >&#xE254;</i></a>';
-                echo '<a href="#deleteUtiizador" onclick="this" class="delete"><i class="material-icons" title="Delete">&#xE872;</i></a>';
+                echo '<a href="#deleteUtiizador" name="deleteUtilizador" class="delete"><i class="material-icons" title="Delete">&#xE872;</i></a>';
                 echo '</td>';
                 echo '</tr>';
             }
             $BFetch->closeCursor();
             $POD = null;
 
-            ?>
+            */?>
 
             <!-- The Modal -->
             <div class="modal fade" id="myModal">
@@ -57,7 +93,7 @@
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                         </div>
 
-                        <div class="modal-body" >
+                        <div class="modal-body" method="Post" >
 
                             <label for="Nome" ><b > Nome</b ></label >
                             <input class="form-control" type = "text" placeholder = "Enter Name" name = "Nome">
@@ -89,6 +125,7 @@
 
                                 <br>
 
+                            <button type="button" class="btn btn-primary" name="Create">Create</button>
                             <button type="button" class="btn btn-primary" name="update">Save changes</button>
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         </div >
@@ -102,13 +139,17 @@
         <div class="clearfix">
             <div class="hint-text">Showing <b>1</b> out of <b>25</b> entries</div>
             <ul class="pagination">
-                <li class="page-item disabled"><a href="#">Previous</a></li>
-                <li class="page-item active"><a href="#" class="page-link">1</a></li>
-                <li class="page-item"><a href="#" class="page-link">2</a></li>
-                <li class="page-item"><a href="#" class="page-link">3</a></li>
-                <li class="page-item"><a href="#" class="page-link">4</a></li>
-                <li class="page-item"><a href="#" class="page-link">5</a></li>
-                <li class="page-item"><a href="#" class="page-link">Next</a></li>
+                <li class="page-item"><a>Previous</a></li>
+                <li class="page-item active" name="pagina" value="0"><a>0</a></li>
+                <li class="page-item"><a>Next</a></li>
+                <button type="button" name="nextpage">Next</button>
+                <?php
+                if(isset($nextpage)){
+                    $paginaanterior=$_POST["pagina"];
+                    $paginaatual=$paginaanterior+1;
+                    $_POST["pagina"]=$paginaatual;
+                }
+                ?>
             </ul>
         </div>
     </div>
