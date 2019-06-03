85b3d23309ee606877d0ad43277f8aa315cf151a
Pedro Miguel Ferreira Ribeiro <a37556@alunos.ipb.pt>
2019-06-03 16:12:22 +0100

    Muita coisa feita gg AdminMenu

---
+++ _PL/Admin/AdminMenu.php
@@ -0,0 +1,31 @@
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <span class="glyphicon glyphicon-bookmark"></span> Quick Shortcuts</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-6">
+                            <a href="?page=Admin/admingames" class="btn btn-danger btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Jogos</a>
+                            <!--<a href="?page=Admin/" class="btn btn-warning btn-lg" role="button"><span class="glyphicon glyphicon-bookmark"></span> <br/>Bookmarks</a>-->
+                            <a href="?page=Admin/Admin_Form" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-signal"></span> <br/>**Forms</a>
+                            <a href="?page=Admin/adminencomendas" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-comment"></span> <br/>Encomendas</a>
+                        </div>
+                        <div class="col-xs-6 col-md-6">
+                            <a href="?page=Admin/adminclientess" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-user"></span> <br/>Users</a>
+                            <!--<a href="?page=Admin/" class="btn btn-info btn-lg" role="button"><span class="glyphicon glyphicon-file"></span> <br/>Notes</a>-->
+                            <a href="?page=Admin/adminplataforma" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-picture"></span> <br/>Photos</a>
+                            <a href="?page=Admin/admingeneros" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-tag"></span><br/>Generos-Plataformas</a>
+                        </div>
+                    </div>
+                    <a href="?page=MainPage" class="btn btn-success btn-lg btn-block" role="button"><span class="glyphicon glyphicon-globe"></span> Website</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
